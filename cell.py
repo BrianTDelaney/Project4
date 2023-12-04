b1d0f6d88d618c74dc8dc3c1875af02255160676
BrianTDelaney <brian.delaney@ufl.edu>
2023-12-04 17:17:50 -0500

    Update cell.py

--- cell.py
+++ cell.py
@@ -7,7 +7,8 @@ class Cell:
         self.row = row
         self.col = col
         self.screen = screen
-        self.sketched = False
+        self.sketched_value = 0
+        self.selected = False
         # Constructor for the Cell class
 
     def set_cell_value(self, value):
@@ -15,7 +16,7 @@ class Cell:
         # Setter for this cell's value
 
     def set_sketched_value(self, value):
-        self.sketched = value
+        self.sketched_value = value
 
     def draw(self):  # Draws this cell, along with the value inside it.
         width = self.screen.get_width()
@@ -26,7 +27,7 @@ class Cell:
         right = (width / 9) * (self.col + 1)
         bottom = (width / 9) * (self.row + 1)
         # based on the size of the screen, calculates the sides of the cell
-        if self.sketched:
+        if self.selected:
             outline_color = (255, 0, 0)
         else:
             outline_color = (0, 0, 0)
@@ -38,5 +39,5 @@ class Cell:
             4
         )
         if self.value != 0:  # If this cell has a nonzero value, that value is displayed. # Otherwise, no value is displayed in the cell.
-            cell_number = pygame.font.SysFont("Arial", 30).render('3', True, (0, 0, 0))  # Creates the number text object that will be displayed
+            cell_number = pygame.font.SysFont("Arial", 30).render(str(self.sketched_value), True, (0, 0, 0))  # Creates the number text object that will be displayed
             self.screen.blit(cell_number, ((left + right / 2), (top + bottom / 2)))  # Actually displays said number
