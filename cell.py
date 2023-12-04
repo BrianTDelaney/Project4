f97093c915010640e17e1e6f7548a00ed3d65a5c
BrianTDelaney <brian.delaney@ufl.edu>
2023-12-04 17:08:14 -0500

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,9 +1,13 @@
+import pygame
+
+
 class Cell:
     def __init__(self, value, row, col, screen):
         self.value = value
         self.row = row
         self.col = col
         self.screen = screen
+        self.sketched = False
         # Constructor for the Cell class
 
     def set_cell_value(self, value):
@@ -11,11 +15,28 @@ class Cell:
         # Setter for this cell's value
 
     def set_sketched_value(self, value):
-        pass
+        self.sketched = value
 
-    def draw(self):
-        pass
-        # Draws this cell, along with the value inside it.
-        # If this cell has a nonzero value, that value is displayed.
-        # Otherwise, no value is displayed in the cell.
-        # The cell is outlined red if it is currently selected.
+    def draw(self):  # Draws this cell, along with the value inside it.
+        width = self.screen.get_width()
+        height = self.screen.get_height()
+        # grabs the size of the screen the cell is to be printed on
+        left = (width / 9) * self.col
+        top = (height / 9) * self.row
+        right = (width / 9) * (self.col + 1)
+        bottom = (width / 9) * (self.row + 1)
+        # based on the size of the screen, calculates the sides of the cell
+        if self.sketched:
+            outline_color = (255, 0, 0)
+        else:
+            outline_color = (0, 0, 0)
+        # changes outline color based on whether the cell has been selected
+        pygame.draw.rect(
+            self.screen,
+            outline_color,
+            (left, top, right, bottom),
+            4
+        )
+        if self.value != 0:  # If this cell has a nonzero value, that value is displayed. # Otherwise, no value is displayed in the cell.
+            cell_number = pygame.font.SysFont("Arial", 30).render('3', True, (0, 0, 0))  # Creates the number text object that will be displayed
+            self.screen.blit(cell_number, ((left + right / 2), (top + bottom / 2)))  # Actually displays said number
