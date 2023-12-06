0a5e518a941e9ef6435626f1079e7b8085d1c21c
BrianTDelaney <brian.delaney@ufl.edu>
2023-12-06 15:35:39 -0500

    Fixed mid-game restart button.

--- sudoku.py
+++ sudoku.py
@@ -130,121 +130,124 @@ def main():
 
     pygame.display.set_caption("Welcome to Sudoku")
 
-    # draw start screen
-    draw_game_start(screen)
-
-    screen.fill(BG_COLOR)
-
-    # make board
-    if diff == 1:
-        board = Board(BOARD_WIDTH, BOARD_HEIGHT, screen, "easy")
-        key, sudoku = generate_sudoku(9, 30)
-        board.set_key(key)
-        board.set_board(sudoku)
-    elif diff == 2:
-        board = Board(BOARD_WIDTH, BOARD_HEIGHT, screen, "medium")
-        key, sudoku = generate_sudoku(9, 40)
-        board.set_key(key)
-        board.set_board(sudoku)
-    elif diff == 3:
-        board = Board(BOARD_WIDTH, BOARD_HEIGHT, screen, "hard")
-        key, sudoku = generate_sudoku(9, 50)
-        board.set_key(key)
-        board.set_board(sudoku)
-    # draw board
-    board.draw()
-
-    # reset, restart, exit buttons
-    button_font = pygame.font.Font(None, 30)
-    reset_text = button_font.render("RESET", 0, (255, 255, 255))
-    restart_text = button_font.render("RESTART", 0, (255, 255, 255))
-    exit_text = button_font.render("EXIT", 0, (255, 255, 255))
-
-    reset_surface = pygame.Surface((reset_text.get_size()[0] + 20, reset_text.get_size()[1] + 20))
-    reset_surface.fill(LINE_COLOR)
-    reset_surface.blit(reset_text, (10, 10))
-    restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
-    restart_surface.fill(LINE_COLOR)
-    restart_surface.blit(restart_text, (10, 10))
-    exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
-    exit_surface.fill(LINE_COLOR)
-    exit_surface.blit(exit_text, (10, 10))
-
-    # initialize difficulty buttons' rectangles
-    reset_rectangle = reset_surface.get_rect(
-        center=(WIDTH // 6, HEIGHT - 100))
-    restart_rectangle = restart_surface.get_rect(
-        center=((WIDTH // 3) + 20, HEIGHT - 100))
-    exit_rectangle = exit_surface.get_rect(
-        center=((WIDTH // 4 * 2) + 40, HEIGHT - 100))
-
-    # draw buttons
-    screen.blit(reset_surface, reset_rectangle)
-    screen.blit(restart_surface, restart_rectangle)
-    screen.blit(exit_surface, exit_rectangle)
-
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # reset button clicked
-                if reset_rectangle.collidepoint(event.pos):
-                    board.reset_to_original()
-                    board.draw()
-                # restart button clicked
-                elif restart_rectangle.collidepoint(event.pos):
-                    draw_game_start(screen)
-                    board.draw()
-                # exit button clicked
-                elif exit_rectangle.collidepoint(event.pos):
+        # draw start screen
+        draw_game_start(screen)
+
+        screen.fill(BG_COLOR)
+
+        # make board
+        if diff == 1:
+            board = Board(BOARD_WIDTH, BOARD_HEIGHT, screen, "easy")
+            key, sudoku = generate_sudoku(9, 30)
+            board.set_key(key)
+            board.set_board(sudoku)
+        elif diff == 2:
+            board = Board(BOARD_WIDTH, BOARD_HEIGHT, screen, "medium")
+            key, sudoku = generate_sudoku(9, 40)
+            board.set_key(key)
+            board.set_board(sudoku)
+        elif diff == 3:
+            board = Board(BOARD_WIDTH, BOARD_HEIGHT, screen, "hard")
+            key, sudoku = generate_sudoku(9, 50)
+            board.set_key(key)
+            board.set_board(sudoku)
+        # draw board
+        board.draw()
+
+        # reset, restart, exit buttons
+        button_font = pygame.font.Font(None, 30)
+        reset_text = button_font.render("RESET", 0, (255, 255, 255))
+        restart_text = button_font.render("RESTART", 0, (255, 255, 255))
+        exit_text = button_font.render("EXIT", 0, (255, 255, 255))
+
+        reset_surface = pygame.Surface((reset_text.get_size()[0] + 20, reset_text.get_size()[1] + 20))
+        reset_surface.fill(LINE_COLOR)
+        reset_surface.blit(reset_text, (10, 10))
+        restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
+        restart_surface.fill(LINE_COLOR)
+        restart_surface.blit(restart_text, (10, 10))
+        exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
+        exit_surface.fill(LINE_COLOR)
+        exit_surface.blit(exit_text, (10, 10))
+
+        # initialize difficulty buttons' rectangles
+        reset_rectangle = reset_surface.get_rect(
+            center=(WIDTH // 6, HEIGHT - 100))
+        restart_rectangle = restart_surface.get_rect(
+            center=((WIDTH // 3) + 20, HEIGHT - 100))
+        exit_rectangle = exit_surface.get_rect(
+            center=((WIDTH // 4 * 2) + 40, HEIGHT - 100))
+
+        # draw buttons
+        screen.blit(reset_surface, reset_rectangle)
+        screen.blit(restart_surface, restart_rectangle)
+        screen.blit(exit_surface, exit_rectangle)
+        main_game = True
+        while main_game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     sys.exit()
-                else:
-                    # get mouse position
-                    x, y = pygame.mouse.get_pos()
-                    clicked_row, clicked_col = board.click(x, y)
-                    # get row and column of mouse position
-                    board.select(clicked_row, clicked_col)
-                    # selects cell at row and column of mouse click
-                    board.draw()
-            if event.type == pygame.KEYDOWN:
-                # checks that the button pressed is a number
-                if event.key in range(49, 58) and board.selected_cell is not None:
-                    board.selected_cell.set_sketched_value(event.key - 48)
-                    # sets selected cell's sketched value to that of the button pressed
-                    board.draw()
-                # checks that a cell is selected and the enter button was pressed
-                if event.key == 13 and board.selected_cell is not None:
-                    if board.selected_cell.sketched_value is not None:
-                        board.selected_cell.set_cell_value(board.selected_cell.sketched_value)
-                        board.selected_cell.set_sketched_value(None)
-                        # sets the selected cell's value equal to the sketched value and clears the sketched value
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # reset button clicked
+                    if reset_rectangle.collidepoint(event.pos):
+                        board.reset_to_original()
                         board.draw()
-                if event.key in range(1073741903, 1073741907):
-                    if board.selected_cell is None:
-                        pass
-                    else:
-                        if event.key == 1073741903:
-                            board.select(board.selected_cell.row, board.next_selectable('right'))
-                        elif event.key == 1073741904:
-                            board.select(board.selected_cell.row, board.next_selectable('left'))
-                        elif event.key == 1073741905:
-                            board.select(board.next_selectable('down'), board.selected_cell.col)
-                        elif event.key == 1073741906:
-                            board.select(board.next_selectable('up'), board.selected_cell.col)
-                        board.draw()
-
-            if board.is_full():
-                # if the player fills the board then it is checked whether they have won or lost
-                if board.check_board():
-                    draw_game_win(screen)
-                    if event.type == pygame.MOUSEBUTTONDOWN:
+                    # restart button clicked
+                    elif restart_rectangle.collidepoint(event.pos):
+                        main_game = False
+                    # exit button clicked
+                    elif exit_rectangle.collidepoint(event.pos):
                         sys.exit()
-                else:
-                    draw_game_over(screen)
+                    else:
+                        # get mouse position
+                        x, y = pygame.mouse.get_pos()
+                        clicked_row, clicked_col = board.click(x, y)
+                        # get row and column of mouse position
+                        board.select(clicked_row, clicked_col)
+                        # selects cell at row and column of mouse click
+                        board.draw()
+                if event.type == pygame.KEYDOWN:
+                    # checks that the button pressed is a number
+                    if event.key in range(49, 58) and board.selected_cell is not None:
+                        board.selected_cell.set_sketched_value(event.key - 48)
+                        # sets selected cell's sketched value to that of the button pressed
+                        board.draw()
+                    # checks that a cell is selected and the enter button was pressed
+                    if event.key == 13 and board.selected_cell is not None:
+                        if board.selected_cell.sketched_value is not None:
+                            board.selected_cell.set_cell_value(board.selected_cell.sketched_value)
+                            board.selected_cell.set_sketched_value(None)
+                            # sets the selected cell's value equal to the sketched value and clears the sketched value
+                            board.draw()
+                    if event.key in range(1073741903, 1073741907):
+                        if board.selected_cell is None:
+                            if board.board[4][4].editable:
+                                board.select(4, 4)
+                            else:
+                                board.select(4, board.next_selectable('right'))
+                        else:
+                            if event.key == 1073741903:
+                                board.select(board.selected_cell.row, board.next_selectable('right'))
+                            elif event.key == 1073741904:
+                                board.select(board.selected_cell.row, board.next_selectable('left'))
+                            elif event.key == 1073741905:
+                                board.select(board.next_selectable('down'), board.selected_cell.col)
+                            elif event.key == 1073741906:
+                                board.select(board.next_selectable('up'), board.selected_cell.col)
+                            board.draw()
 
-        pygame.display.update()
-        # updates screen with any visual changes
+                if board.is_full():
+                    # if the player fills the board then it is checked whether they have won or lost
+                    if board.check_board():
+                        draw_game_win(screen)
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            sys.exit()
+                    else:
+                        draw_game_over(screen)
+
+            pygame.display.update()
+            # updates screen with any visual changes
 
 
 if __name__ == '__main__':
